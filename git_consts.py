3fee56f5762832690031a7df35fbea766b0ecac9
Alexandra Lomotan <alomotan@splunk.com>
2018-12-11 14:54:33 -0800

    PAPP-5592 NRI, update cr, update phantom min version

--- git_consts.py
+++ git_consts.py
@@ -1,16 +1,8 @@
-# --
 # File: git_consts.py
+# Copyright (c) 2017-2018 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2017-2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber Corporation.
-#
-# --
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 
 GIT_CONFIG_REPO_URI = "repo_uri"
 GIT_CONFIG_REPO_NAME = "repo_name"
