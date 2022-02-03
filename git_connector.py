ad7b31d671b2b8b9c5841f913c8b628a7705c9ce
Ronald Gil <rgil@splunk.com>
2022-02-02 16:40:29 -0800

    Pipeline fixes

--- git_connector.py
+++ git_connector.py
@@ -15,21 +15,21 @@
 #
 #
 # Standard library imports
+import ast
 import json
 import os
-import git
-import ast
 import urllib.parse
-from Cryptodome.PublicKey import RSA
 from pathlib import Path
 from shutil import rmtree
 
 # Phantom imports
 import phantom.app as phantom
-from phantom.base_connector import BaseConnector
-from phantom.action_result import ActionResult
 import phantom.rules as phantom_rules
+from Cryptodome.PublicKey import RSA
+from phantom.action_result import ActionResult
+from phantom.base_connector import BaseConnector
 
+import git
 # Local imports
 import git_consts as consts
 
@@ -569,7 +569,9 @@ class GitConnector(BaseConnector):
         # Create temp pub_key to add to the vault
         pub_key_vault_path = ssh_key_dir / 'id_rsa.pub_vault'
         pub_key_vault_path.write_bytes(pub_key)
-        status, message, vault_id = phantom_rules.vault_add(container=self.get_container_id(), file_location=str(pub_key_vault_path), file_name='id_rsa.pub')
+        status, message, vault_id = phantom_rules.vault_add(container=self.get_container_id(),
+                                                            file_location=str(pub_key_vault_path),
+                                                            file_name='id_rsa.pub')
         if not status:
             return action_result.set_status(phantom.APP_ERROR,
                                             'Error adding file to vault: {}'.format(message))
@@ -739,12 +741,13 @@ class GitConnector(BaseConnector):
 if __name__ == '__main__':
 
     import sys
+
     import pudb
 
     pudb.set_trace()
     if len(sys.argv) < 2:
         print('No test json specified as input')
-        exit(1)
+        sys.exit(1)
     json_path = Path(sys.argv[1]).read_text()
     in_json = json.loads(json_path)
     print(json.dumps(in_json, indent=4))
@@ -752,4 +755,4 @@ if __name__ == '__main__':
     connector.print_progress_message = True
     return_value = connector._handle_action(json.dumps(in_json), None)
     print(json.dumps(json.loads(return_value), indent=4))
-    exit(0)
+    sys.exit(0)
