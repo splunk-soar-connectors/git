1afbf6bd9fed8859dbe200d53df7be844439da25
HarlodHolt <67050609+HarlodHolt@users.noreply.github.com>
2024-02-10 08:01:07 +1100

    Fix issue #12 byte-based instead of text based

--- git_connector.py
+++ git_connector.py
@@ -247,8 +247,8 @@ class GitConnector(BaseConnector):
                     return action_result.set_status(phantom.APP_ERROR, f"Unable to get vault_info: {message}")
 
                 try:
-                    vault_file_path = Path(next(iter(vault_file_info)).get("path"))
-                    vault_file_data = vault_file_path.read_text()
+                    vault_file_path = Path(list(vault_file_info)[0].get('path'))
+                    vault_file_data = vault_file_path.read_bytes()
                 except Exception as e:
                     self.debug_print(f"Exception : {e}")
                     return action_result.set_status(phantom.APP_ERROR)
@@ -264,7 +264,7 @@ class GitConnector(BaseConnector):
             full_path.parent.mkdir(parents=True, exist_ok=True)
 
             # overwrite file into local disk
-            full_path.write_text(file_data)
+            full_path.write_bytes(file_data)
 
             # add into index
             repo.index.add(file_path)
